38a2d4e33a37259bacb0f3399c35ec45e6891c60
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2026-05-13 11:56:48 -0500

    refactor(config): move database credentials to .env and improve environment setup.

--- config/database.php
+++ config/database.php
@@ -2,13 +2,20 @@
 
 <?php
 
-$host = "localhost";
-$dbname = "phi_crud";
-$username = "root";
-$password = "";
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+$host = $env['DB_HOST'];
+$dbname = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
